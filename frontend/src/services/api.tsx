737c88dd53643ce7f14b6a52845091ecd2fc5d12
Lennox Gonzalez <lennoxgonz@gmail.com>
2025-03-16 14:20:39 -0400

    updated docker compose file

--- frontend/src/services/api.tsx
+++ frontend/src/services/api.tsx
@@ -1,8 +1,8 @@
 import axios from "axios";
 
-//const API_BASE_URL: string = 'http://localhost:8080/api';
-const API_BASE_URL: string =
-  "https://8080-lennoxgonz-scubagearpic-t5oybvaq8uc.ws-us118.gitpod.io/api";
+const API_BASE_URL: string = 'http://localhost:8080/api';
+//const API_BASE_URL: string =
+//  "https://8080-lennoxgonz-scubagearpic-t5oybvaq8uc.ws-us118.gitpod.io/api";
 
 export async function fetchAllGear() {
   try {
